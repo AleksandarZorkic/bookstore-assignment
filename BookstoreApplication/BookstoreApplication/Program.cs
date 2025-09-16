b136ecd5d4b61db29264a8e0c39bfbd6d2431e8f
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-09-16 22:06:13 +0200

    Tabele su kreirane.

--- BookstoreApplication/BookstoreApplication/Program.cs
+++ BookstoreApplication/BookstoreApplication/Program.cs
@@ -1,23 +1,35 @@
+using BookstoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// EF Core + Npgsql
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
+// MVC kontroleri
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS (dev)
+builder.Services.AddCors(o => o.AddDefaultPolicy(p =>
+    p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();

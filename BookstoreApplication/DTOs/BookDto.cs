b192b678d82fe688fdd8e6243be4ec1f3971cfda
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-10-09 19:12:14 +0200

    Uradjen prvi deo zadatka: Dodavanje DTO i AutoMapper.

---
+++ BookstoreApplication/DTOs/BookDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BookstoreApplication.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string AuthorFullName { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublisheDate { get; set; }
+        public int YearsAgo => Math.Max(0, DateTime.Now.Year - PublisheDate.Year);
+    }
+}

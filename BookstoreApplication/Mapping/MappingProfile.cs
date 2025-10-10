94b8e84cc2963fb50dc9b6af9b151c55310f90bd
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-10-10 19:27:11 +0200

    Ispravljena verzija AutoMappera.

--- BookstoreApplication/Mapping/MappingProfile.cs
+++ BookstoreApplication/Mapping/MappingProfile.cs
@@ -9,16 +9,15 @@ namespace BookstoreApplication.Mapping
         public MappingProfile()
         {
             CreateMap<Book, BookDto>()
-                .ForMember(d => d.AuthorFullName, m => m.MapFrom(s => s.Author.FullName))
-                .ForMember(d => d.PublisherName, m => m.MapFrom(s => s.Publisher.Name))
-                .ForMember(d => d.YearsAgo, m => m.MapFrom(s => s.PublishedDate));
+                .ForMember(d => d.AuthorFullName, m => m.MapFrom(s => s.Author != null ? s.Author.FullName : ""))
+                .ForMember(d => d.PublisherName, m => m.MapFrom(s => s.Publisher != null ? s.Publisher.Name : ""))
+                .ForMember(d => d.YearsAgo, m => m.MapFrom(s => YearsSince(s.PublishedDate)));
 
             CreateMap<Book, BookDetailsDto>()
-                .ForMember(d => d.AuthorId, m => m.MapFrom(s => s.AuthorId))
-                .ForMember(d => d.AuthorFullName, m => m.MapFrom(s => s.Author.FullName))
-                .ForMember(d => d.PublisherId, m => m.MapFrom(s => s.PublisherId))
-                .ForMember(d => d.PublisherName, m => m.MapFrom(s => s.Publisher.Name));
+                .ForMember(d => d.AuthorFullName, m => m.MapFrom(s => s.Author != null ? s.Author.FullName : ""))
+                .ForMember(d => d.PublisherName, m => m.MapFrom(s => s.Publisher != null ? s.Publisher.Name : ""));
         }
+
         private static int YearsSince(DateTime date)
         {
             var now = DateTime.UtcNow.Date;

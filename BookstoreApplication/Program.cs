8d730e02a15b22ce7d0c7739a921d02b3a3b0332
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-11-03 13:23:55 +0100

    Uradio sam zadatak: Uvođenje Identity radnog okvira.

--- BookstoreApplication/Program.cs
+++ BookstoreApplication/Program.cs
@@ -1,4 +1,4 @@
-using BookstoreApplication.Mapping;
+﻿using BookstoreApplication.Mapping;
 using Microsoft.EntityFrameworkCore;
 using BookstoreApplication;
 using BookstoreApplication.Repositories.Interfaces;
@@ -10,6 +10,7 @@ using BookstoreApplication.Middleware;
 using Serilog;
 using AutoMapper;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,22 +21,31 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Host.UseSerilog();
 
-// EF Core + Npgsql
+// 1) EF Core + Npgsql
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-// MVC kontroleri
-builder.Services.AddControllers();
+// 2) Controllers (jednom) + enum kao string
+builder.Services.AddControllers()
+    .AddJsonOptions(o =>
+    {
+        o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
-// Swagger
+// 3) Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS (dev)
-builder.Services.AddCors(o => o.AddDefaultPolicy(p =>
-    p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+// 4) CORS za SPA 
+builder.Services.AddCors(o => o.AddPolicy("spa", p =>
+    p.WithOrigins("http://localhost:5173")
+     .AllowAnyHeader()
+     .AllowAnyMethod()
+     .AllowCredentials()
 ));
+
+// 5) Repozitorijumi/servisi
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
 builder.Services.AddScoped<IAwardRepository, AwardRepository>();
@@ -46,18 +56,28 @@ builder.Services.AddScoped<IPublisherService, PublisherService>();
 builder.Services.AddScoped<IAwardService, AwardService>();
 builder.Services.AddScoped<IBookService, BookService>();
 
-builder.Services.AddAutoMapper(typeof(BookstoreApplication.Mapping.MappingProfile).Assembly);
-
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 builder.Services.AddGlobalExceptionHandling();
 
-builder.Services.AddControllers()
-    .AddJsonOptions(o =>
-    {
-        o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+// 6) Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+    opt.Password.RequireDigit = true;
+    opt.Password.RequireLowercase = true;
+    opt.Password.RequireUppercase = true;
+    opt.Password.RequireNonAlphanumeric = true;
+    opt.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+// Validacija AutoMapper profila
 using (var scope = app.Services.CreateScope())
 {
     var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
@@ -70,10 +90,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseGlobalExceptionHandling();
 
-app.UseCors();
+app.UseHttpsRedirection();
+
+app.UseCors("spa");
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
---
+++ BookstoreApplication/Services/Implementations/AuthService.cs
@@ -0,0 +1,54 @@
+﻿using BookstoreApplication.DTOs.UserDto;
+using BookstoreApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using BookstoreApplication.Services.Interfaces;
+
+namespace BookstoreApplication.Services.Implementations
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthService(UserManager<ApplicationUser> um, SignInManager<ApplicationUser> sm)
+        {
+            _userManager = um;
+            _signInManager = sm;
+        }
+
+        public async Task RegisterAsync(RegistrationDto data)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = data.UserName,
+                Email = data.Email,
+                Name = data.Name,
+                Surname = data.Surname,
+            };
+
+            var result = await _userManager.CreateAsync(user, data.Password);
+            if (!result.Succeeded)
+            {
+                var msg = string.Join("; ", result.Errors.Select(e => e.Description));
+                throw new BadHttpRequestException(msg);
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: true);
+        }
+
+        public async Task LoginAsync(LoginDto data)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                userName: data.Username,
+                password: data.Password,
+                isPersistent: true,
+                lockoutOnFailure: false);
+
+            if (!result.Succeeded)
+                throw new BadHttpRequestException("Invalid credentials.");
+        }
+
+        public async Task LogoutAsync() => _signInManager.SignOutAsync();
+    }
+        
+}
